45752e3335717d5aa389f455401b8cb099fd3c37
Gordon <gschmitz22@gmail.com>
2017-12-13 19:58:13 -0600

    lot of additional features

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,26 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+  def new
+    @category = Category.new
+  end
+  def edit
+    @category = Category.find_by(params[:id])
+  end
+  def update
+    category = category_params[:category]
+    Category.update(category_params[:id], name: category[:name], description: category[:description])
+    redirect_to categories_path
+  end
+  def create
+    if Account.new(category_params[:category]).save
+      redirect_to categories_path
+    else
+      redirect_to new_category_path
+    end
+  end
+
+  def category_params
+    params.permit(:id, category: [:name, :description])
+  end
 end
