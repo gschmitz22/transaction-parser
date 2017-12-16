d51008a4e456ba363a168d7a8a7f95d95788e46e
Gordon <gschmitz22@gmail.com>
2017-12-16 15:42:19 -0600

    Bootstrap overhaul

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   resources :accounts
   resources :categories
+  resources :paychecks
 
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
